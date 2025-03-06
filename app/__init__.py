7c73f002b550dde78ed317f73825ecb6fd2c2a21
Mayen <139054881+Mayen007@users.noreply.github.com>
2025-03-06 10:25:49 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import datetime
 
-
 db = SQLAlchemy()
 migrate = None
 
-
 def create_app():
-    app = Flask(__name__, template_folder='../templates',
-                static_folder='../static')
+    app = Flask(__name__, template_folder='../templates', static_folder='../static')
     app.config['SECRET_KEY'] = 'supersecretkey'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
